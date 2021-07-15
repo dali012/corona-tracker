ba3cfdfec707c3a6fe5a6bfd92cbb797d08b0f8b
dali012 <dali012@DESKTOP-GDQ9R31.localdomain>
2021-07-15 23:09:52 +0100

    corona tracker

---
+++ src/api/index.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const URL = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) => {
+  let changeableURL = URL;
+  if(country) {
+    changeableURL = `${URL}/countries/${country}`;
+  }
+    try {
+        const { data: {confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableURL);
+        return { confirmed, recovered, deaths, lastUpdate };
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${URL}/daily`);
+
+    return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries } } = await axios.get(`${URL}/countries`);
+    return countries.map((country) => country.name);
+  } catch (error) {
+    console.log(error);
+  }
+};
